78bbcad9ae79b0f979ef2c357ddc1fda756ed33d
Anusha S <anushasridharan7@gmail.com>
2023-04-06 22:10:11 +0530

    code for bg removal

--- extract-img-text.py
+++ extract-img-text.py
@@ -1,4 +1,3 @@
 from PIL import Image
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 print(pytesseract.image_to_string(Image.open('image.png')))
